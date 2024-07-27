6f3ff304927737c16b9923ed6dbd42e443d31a08
Nicolas Powell <nicopowell30@gmail.com>
2024-07-27 16:34:29 -0300

    agregada funcion chauAgus

--- desafio12/desafio12.js
+++ desafio12/desafio12.js
@@ -1,8 +1,8 @@
 const texto = "Hola mundo";
-const comandos = ["INTERCAMBIA",  "BORRAULTI"];
+const comandos = ["INTERCAMBIA", "BORRAULTI"];
 
 function procesatexto(texto, comandos) {
-    let cadenaProcesada = texto
+    let cadenaProcesada = texto;
     function comprobarMemoria(texto) {
         if (texto.length > 1000) {
             return "MemoryLimitExceeded";
@@ -41,47 +41,49 @@ function procesatexto(texto, comandos) {
         }
     }
 
-    function borraUlti(texto){
-        cadenaProcesada = texto.substring(0, texto.length - 1)
+    function borraUlti(texto) {
+        cadenaProcesada = texto.substring(0, texto.length - 1);
     }
 
-    function borraPri(texto){
-        cadenaProcesada = texto.substring(1, texto.length)
+    function borraPri(texto) {
+        cadenaProcesada = texto.substring(1, texto.length);
     }
 
-    function dup(texto){
-        cadenaProcesada = texto + texto
+    function dup(texto) {
+        cadenaProcesada = texto + texto;
     }
 
     function rota(texto) {
-        if(texto.lenght>0){
-        let caracter = texto.charAt(0);
-        texto = texto.substring(1, texto.lenght).concat(caracter)
+        if (texto.lenght > 0) {
+            let caracter = texto.charAt(0);
+            cadenaProcesada = texto.substring(1, texto.lenght).concat(caracter);
         }
     }
 
-    function intercambia(texto){
-        const vec=texto.split('');
-        for(let i=0;i<vec.length;i++){
-          let ch=vec[i]
-          if(ch===ch.toUpperCase()){
-            vec[i]=ch.toLowerCase();
-          }else{
-            vec[i]=ch.toUpperCase()
-          }
+    function intercambia(texto) {
+        const vec = texto.split("");
+        for (let i = 0; i < vec.length; i++) {
+            let ch = vec[i];
+            if (ch === ch.toUpperCase()) {
+                vec[i] = ch.toLowerCase();
+            } else {
+                vec[i] = ch.toUpperCase();
+            }
         }
-        return texto=vec.join('')
-      }
+        return (cadenaProcesada = vec.join(""));
+    }
 
-    function chauAgus(texto){
-        let mayuscula = texto.toUpperCase
-        if(mayuscula.includes("AGUS")){
-            cadenaProcesada = texto.slice(mayuscula.indexOf("AGUS"), mayuscula.indexOf("AGUS") + 4)
+    function chauAgus(texto) {
+        let mayuscula = texto.toUpperCase;
+        if (mayuscula.includes("AGUS")) {
+            cadenaProcesada = texto.substring(0, mayuscula.indexOf("AGUS")) + texto.substring(mayuscula.indexOf("AGUS") + 4, texto.length);
         }
     }
 
-    for(let i = 0; i < comandos.length; i++){
-        comprobarMemoria(texto)
-        elegirFuncion(comandos[i])
+    for (let i = 0; i < comandos.length; i++) {
+        comprobarMemoria(texto);
+        elegirFuncion(comandos[i]);
     }
+
+    return cadenaProcesada;
 }
