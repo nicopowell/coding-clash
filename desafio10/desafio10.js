f7c6099669cac2842eb910b364bfb46604f89085
Nicolas Powell <nicopowell30@gmail.com>
2024-07-27 15:31:46 -0300

    se borraron las variables de prueba y se dio formato al codigo

--- desafio10/desafio10.js
+++ desafio10/desafio10.js
@@ -1,39 +1,35 @@
-const K = 5
-const N = [1, 1, 1, 1, 1]
-
-function chocolates(K, N){
-    let cantidadVariedades
-    let caja = 0
-    let cantidadCajas = 0
-    if(K > N.length){
-        return cantidadCajas
+function chocolates(K, N) {
+    let cantidadVariedades;
+    let caja = 0;
+    let cantidadCajas = 0;
+    if (K > N.length || K <= 0) {
+        return cantidadCajas;
     }
 
-
-    function variedades(N){
-        cantidadVariedades = 0
+    function variedades(N) {
+        cantidadVariedades = 0;
         for (let i = 0; i < N.length; i++) {
-            if(N[i] > 0){
-                cantidadVariedades++
+            if (N[i] > 0) {
+                cantidadVariedades++;
             }
         }
-    }      
-
-    variedades(N)
-
-    while(cantidadVariedades >= K){
-        for (let i = 0; i < N.length; i++) {
-            if(N[i]>0){
-                caja++
-                N[i]--
-            }
-            if(caja === 5){
-                cantidadCajas++
-                caja = 0
-                break
-            }
-        }
-        variedades(N)
     }
-    return cantidadCajas
+
+    variedades(N);
+
+    while (cantidadVariedades >= K) {
+        for (let i = 0; i < N.length; i++) {
+            if (N[i] > 0) {
+                caja++;
+                N[i]--;
+            }
+            if (caja === K) {
+                cantidadCajas++;
+                caja = 0;
+                break;
+            }
+        }
+        variedades(N);
+    }
+    return cantidadCajas;
 }
