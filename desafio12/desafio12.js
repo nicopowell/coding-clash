d733ed7ec21ac138acea88e680645a18433cff81
Nicolas Powell <nicopowell30@gmail.com>
2024-07-27 15:45:19 -0300

    se creo la funcion que ejecuta la funcion requerida

--- desafio12/desafio12.js
+++ desafio12/desafio12.js
@@ -1,10 +1,42 @@
-const texto = 'Hola mundo'
-const comandos = ['INTERCAMBIA', 'BORRAULTI']
+const texto = "Hola mundo";
+const comandos = ["INTERCAMBIA", "BORRAULTI"];
 
-function procesatexto(texto, comandos){
-    function comprobarMemoria(texto){
-        if(texto.length > 1000){
-            return 'MemoryLimitExceeded'
+function procesatexto(texto, comandos) {
+    function comprobarMemoria(texto) {
+        if (texto.length > 1000) {
+            return "MemoryLimitExceeded";
+        }
+    }
+
+    function elegirFuncion(comando) {
+        switch (comando) {
+            case "INTERCAMBIA":
+                intercambia();
+                break;
+            case "BORRAULTI":
+                borraUlti();
+                break;
+            case "BORRAPRI":
+                borraPri();
+                break;
+            case "DUP":
+                dup();
+                break;
+            case "ROTA":
+                rota();
+                break;
+            case "INVERTIR":
+                invertir();
+                break;
+            case "CHAUAGUS":
+                chauAgus();
+                break;
+            case "AGREGA":
+                agrega();
+                break;
+            case "BORRA":
+                borra();
+                break;
         }
     }
 }
