969b1c2c8c1e6f645cb6525e3aad8c9e5e5e9b6f
Nicolas Powell <nicopowell30@gmail.com>
2024-07-27 15:37:05 -0300

    se agrega la funcion principal y la funcion para comprobar memoria

---
+++ desafio12/desafio12.js
@@ -0,0 +1,10 @@
+const texto = 'Hola mundo'
+const comandos = ['INTERCAMBIA', 'BORRAULTI']
+
+function procesatexto(texto, comandos){
+    function comprobarMemoria(texto){
+        if(texto.length > 1000){
+            return 'MemoryLimitExceeded'
+        }
+    }
+}
